5e4088e372e1aa610e9f8dc809da07e28a9b2370
IsraelMobote <moboteisrael02@gmail.com>
2024-01-12 04:32:29 +0100

    that

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        string color= Console.ReadLine();
+        Console.WriteLine($"{color} is your color");
     }
 }
