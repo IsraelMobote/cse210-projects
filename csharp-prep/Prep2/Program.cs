b164c6977eaad57a158a3a7bbe13f86c5f3f280d
IsraelMobote <moboteisrael02@gmail.com>
2024-01-12 12:05:31 +0100

    I changed the README.md

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,6 +6,6 @@ class Program
     {
         Console.WriteLine("Hello Prep2 World!");
         string color= Console.ReadLine();
-        Console.WriteLine($"{color} is your color");
+        Console.WriteLine($"{color} is your typed color");
     }
 }
